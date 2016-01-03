a0e243cb5db3d159171c9951241264f382cde4ca
raqbiel <raqbiel@gmail.com>
2016-01-03 15:42:19 +0100

    Baza sql update
    poprawki do bazy danych opcja usuniecia z hasłem

--- SQLDB/gen/com/example/sqlen/R.java
+++ SQLDB/gen/com/example/sqlen/R.java
@@ -27,25 +27,27 @@ public final class R {
         public static final int miasto=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
+        public static final int EditText_Pwd1=0x7f080011;
+        public static final int TextView_Pwd1=0x7f080010;
+        public static final int TextView_PwdProblem=0x7f080012;
+        public static final int action_settings=0x7f080014;
         public static final int btnadd=0x7f080004;
         public static final int btndelete=0x7f080006;
         public static final int btnupdate=0x7f080005;
         public static final int btnview=0x7f080008;
+        public static final int button1=0x7f080013;
         public static final int edcountry=0x7f080003;
         public static final int edplace=0x7f080002;
         public static final int edplaceid=0x7f080001;
-        public static final int haslo=0x7f080011;
         public static final int imageView1=0x7f080007;
-        public static final int lbl_your_name=0x7f08000f;
         public static final int lvh=0x7f08000b;
         public static final int placeslist=0x7f08000a;
+        public static final int root=0x7f08000f;
         public static final int textView1=0x7f080000;
         public static final int txtcountry=0x7f08000e;
         public static final int txtplaceid=0x7f08000c;
         public static final int txtplacename=0x7f08000d;
         public static final int txtresulttext=0x7f080009;
-        public static final int username=0x7f080010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- SQLDB/src/com/example/sqlen/MainActivity.java
+++ SQLDB/src/com/example/sqlen/MainActivity.java
@@ -6,31 +6,45 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 public class MainActivity extends Activity  {
+	
     ObslugaDB dbObsluga = new ObslugaDB(this);
+    Button add, view, update, delete,accept;
+    EditText placeid, place, country,pin,password1;
+    TextView infotext,wiadomosc,textView1;
 
-    
-   
-    Button add, view, update, delete;
-    EditText placeid, place, country,pin;
-    TextView infotext,wiadomosc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);  
         setContentView(R.layout.activity_main);
+
+        
+        
+		
+        password1 = (EditText)findViewById(R.id.EditText_Pwd1);
+       // placeid = (EditText) findViewById(R.id.edplaceid);
+        accept = (Button)findViewById(R.id.button1);
+		textView1 = (TextView)findViewById(R.id.textView1);
+		
         placeid = (EditText) findViewById(R.id.edplaceid);
         place = (EditText) findViewById(R.id.edplace);
         country = (EditText) findViewById(R.id.edcountry);
@@ -39,7 +53,7 @@ public class MainActivity extends Activity  {
         delete = (Button) findViewById(R.id.btndelete);
         view = (Button) findViewById(R.id.btnview);
         infotext = (TextView) findViewById(R.id.txtresulttext);
-		pin = (EditText)findViewById(R.id.username);
+
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,33 +105,34 @@ public class MainActivity extends Activity  {
                 }
             }
         });
-    
-       /* delete.setOnClickListener(new View.OnClickListener() {
+    }
+        
+     /*   delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
                     if (placeid.getText().toString().trim().equals("")) {
-                        infotext.setText("Please insert place ID to delete..");
+                        infotext.setText("Wpisz ID do usuniêcia...");
                     } else {
                     	dbObsluga = new ObslugaDB(getApplicationContext());
                         SQLiteDatabase db = dbObsluga.getWritableDatabase();
                         db.delete("miejsca", "id=" + placeid.getText().toString(), null);
                         Toast.makeText(MainActivity.this,
-                                "deleted successfully", Toast.LENGTH_SHORT)
+                                "usuniêty poprawnie!", Toast.LENGTH_SHORT)
                                 .show();
-                        infotext.setText("Deleted Successfully");
+                        infotext.setText("Usuniêty poprawnie!");
                     }
                 } catch (Exception ex) {
                     infotext.setText(ex.getMessage().toString());
                 }
             }
-        });*/
-    }
+        });
+    }*/
         @SuppressLint("NewApi")
 		public void wybor (View arg0){
-    		AlertDialog.Builder budowa = new AlertDialog.Builder(MainActivity.this);
+    		final AlertDialog.Builder budowa = new AlertDialog.Builder(MainActivity.this);
     		
-    		
+ 
     		TextView wiadomosc = new TextView(this);
     		wiadomosc.setText("Czy na pewno chcesz usun¹æ?");
     		wiadomosc.setGravity(Gravity.CENTER_HORIZONTAL);
@@ -127,30 +142,65 @@ public class MainActivity extends Activity  {
     		budowa.setCancelable(false);
     		budowa.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
     			
-    			@Override
-    			public void onClick(DialogInterface dialog, int which) {
-    				try {
-                        if (placeid.getText().toString().trim().equals("")) {
-                            infotext.setText("Wpisz ID, aby usun¹æ..");
+    			public void onClick(DialogInterface dialog, int which) {	
+    				setContentView(R.layout.user_name);
+    			    accept = (Button)findViewById(R.id.button1);
+    			    password1 = (EditText)findViewById(R.id.EditText_Pwd1);
+    			    textView1 = (TextView)findViewById(R.id.textView1);
+    			    
+    			    accept.setOnClickListener(new OnClickListener() {
+						
+						@Override
+						public void onClick(View v) {
+							try {			  
+		    				      if (password1.getText().toString().trim().equals("chuj")) {
+		    				    	  dbObsluga = new ObslugaDB(getApplicationContext());
+		    			                SQLiteDatabase db = dbObsluga.getWritableDatabase();
+		    			                db.delete("miejsca", "id=" + placeid.getText().toString(), null);
+		    			                Toast.makeText(MainActivity.this,
+		    			                        "Poprawnie Usuniêty", Toast.LENGTH_SHORT)
+		    			                        .show();
+		    			                textView1.setText("Poprawnie Usuniêty!");	
+		    			                Intent i = new Intent(MainActivity.this, MainActivity.class);
+		    			                startActivity(i);
+		    		          } else {
+		    		        		textView1.setText("B£ÊDNE HAS£O!");
+
+		    		                }
+		    		      } catch (Exception ex) {
+		    		    	  textView1.setText(ex.getMessage().toString());
+		    		      }
+		    				
+		    			}
+
+					});
+	
+    		
+    				/*try {
+    					  budowa.setView(R.layout.user_name);
+    					  String strPassword1 = password1.getText().toString();
+    				      String strPassword2 = password2.getText().toString();
+    				      if (strPassword1.equals("chuj")) {
+    				    	  dbObsluga = new ObslugaDB(getApplicationContext());
+                              SQLiteDatabase db = dbObsluga.getWritableDatabase();
+                              db.delete("miejsca", "id=" + placeid.getText().toString(), null);
+                              Toast.makeText(MainActivity.this,
+                                      "Poprawnie Usuniêty", Toast.LENGTH_SHORT)
+                                      .show();
+                              infotext.setText("Poprawnie Usuniêty!");
+
                         } else {
-                        	dbObsluga = new ObslugaDB(getApplicationContext());
-                            SQLiteDatabase db = dbObsluga.getWritableDatabase();
-                            db.delete("miejsca", "id=" + placeid.getText().toString(), null);
-                            Toast.makeText(MainActivity.this,
-                                    "Poprawnie Usuniêty", Toast.LENGTH_SHORT)
-                                    .show();
-                            infotext.setText("Poprawnie Usuniêty!");
+                        	infotext.setText("B£¥D!");
                         }
                     } catch (Exception ex) {
                         infotext.setText(ex.getMessage().toString());
-                    }
-                }
-            });
-    			
+                    }*/
+    		};
+    		});
+    		
 
     		budowa.setNegativeButton("Nie",new DialogInterface.OnClickListener() {
     			
-    			@Override
     			public void onClick(DialogInterface dialog, int which) {
     				dialog.cancel();
     				
@@ -172,4 +222,5 @@ public class MainActivity extends Activity  {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
